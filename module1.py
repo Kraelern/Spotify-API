1f6660fa906c6e4b115958c8231e4c5b00f48ee7
rtan123 <taneranaga45@gmail.com>
2020-04-16 19:03:26 -0400

    creating a test commit
    reset the module and programmed it to print 'test2'

--- module1.py
+++ module1.py
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-import requests
-import time
-
-spotify = https://api.spotify.com
-print(spotify)
-=======
-chars = 'this is an actual test'
-for i in chars:
-    print(i)
-    hhlkhjkjklkk
->>>>>>> b18e2f8643c89dd82588a60ebdcd25401a4dfa93
+print('test2')
