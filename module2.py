e5c87fe7a8cf3ea12ec4d729040c6d7d6c22b596
rtan123 <taneranaga45@gmail.com>
2020-04-17 18:18:05 -0400

    testing authentication code flow
    module 2 contains the tests script for authentication code

---
+++ module2.py
@@ -0,0 +1,44 @@
+import sys
+import spotipy
+import spotipy.util as util
+
+util.prompt_for_user_token('the_ranaga',
+                           'user_playlists',
+                           client_id='898c3a3775d944e2952d43a3bc4dbd55',
+                           client_secret='ba04bb9f22c344f99ba5eb3f8e3d9981',
+                           redirect_uri='https://accounts.spotify.com/en/login/')
+
+def show_tracks(tracks):
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        username = sys.argv[1]
+    else:
+        print("Whoops, need your username!")
+        print("usage: python user_playlists.py [username]")
+        sys.exit()
+
+    token = util.prompt_for_user_token(username)
+
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        playlists = sp.user_playlists(username)
+        for playlist in playlists['items']:
+            if playlist['owner']['id'] == username:
+                print()
+                print(playlist['name'])
+                print ('  total tracks', playlist['tracks']['total'])
+                results = sp.playlist(playlist['id'],
+                    fields="tracks,next")
+                tracks = results['tracks']
+                show_tracks(tracks)
+                while tracks['next']:
+                    tracks = sp.next(tracks)
+                    show_tracks(tracks)
+    else:
+        print("Can't get token for", username)
