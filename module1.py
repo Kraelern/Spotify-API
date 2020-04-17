67309a57e3d7fe285aab371cf4b866a47029c106
rtan123 <taneranaga45@gmail.com>
2020-04-17 17:46:53 -0400

    client credentials flow
    testing the second form of authentication

--- module1.py
+++ module1.py
@@ -1,9 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-cid = '898c3a3775d944e2952d43a3bc4dbd55'
-secret = 'ba04bb9f22c344f99ba5eb3f8e3d9981'
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager
-=
-client_credentials_manager)
+client_credentials_manager = SpotifyClientCredentials('898c3a3775d944e2952d43a3bc4dbd55', 'ba04bb9f22c344f99ba5eb3f8e3d9981')
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists('spotify')
+while playlists:
+    for i, playlist in enumerate(playlists['items']):
+        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
+    if playlists['next']:
+        playlists = sp.next(playlists)
+    else:
+        playlists = None
