5ec46ed2b17bd62a95e9b97b0f9ddd8c55a82aa1
rtan123 <taneranaga45@gmail.com>
2020-04-17 21:25:22 -0400

    adjustments
    trying to grant access through authentication code

--- module2.py
+++ module2.py
@@ -2,43 +2,25 @@ import sys
 import spotipy
 import spotipy.util as util
 
-util.prompt_for_user_token('the_ranaga',
-                           'user-library-read',
-                           client_id='898c3a3775d944e2952d43a3bc4dbd55',
-                           client_secret='ba04bb9f22c344f99ba5eb3f8e3d9981',
-                           redirect_uri='https://accounts.spotify.com/en/login/')
+util.prompt_for_user_token(username= 'the_ranaga',
+                           scope= 'user-library-read',
+                           client_id= '898c3a3775d944e2952d43a3bc4dbd55',
+                           client_secret= 'ba04bb9f22c344f99ba5eb3f8e3d9981',
+                           redirect_uri= 'http://localhost/')
 
-def show_tracks(tracks):
-    for i, item in enumerate(tracks['items']):
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    print("Usage: %s username" % (sys.argv[0],))
+    sys.exit()
+
+token = util.prompt_for_user_token(username, scope)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+    results = sp.current_user_saved_tracks()
+    for item in results['items']:
         track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name']))
-
-
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print("Whoops, need your username!")
-        print("usage: python user_playlists.py [username]")
-        sys.exit()
-
-    token = util.prompt_for_user_token(username)
-
-    if token:
-        sp = spotipy.Spotify(auth=token)
-        playlists = sp.user_playlists(username)
-        for playlist in playlists['items']:
-            if playlist['owner']['id'] == username:
-                print()
-                print(playlist['name'])
-                print ('  total tracks', playlist['tracks']['total'])
-                results = sp.playlist(playlist['id'],
-                    fields="tracks,next")
-                tracks = results['tracks']
-                show_tracks(tracks)
-                while tracks['next']:
-                    tracks = sp.next(tracks)
-                    show_tracks(tracks)
-    else:
-        print("Can't get token for", username)
+        print(track['name'] + ' - ' + track['artists'][0]['name'])
+else:
+    print("Can't get token for", username)
