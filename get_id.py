adb6ed1309be077f8efa4408d9e2fc65970a79f1
rtan123 <taneranaga45@gmail.com>
2020-05-01 10:44:54 -0400

    Update get_id.py

--- get_id.py
+++ get_id.py
@@ -5,13 +5,17 @@ client_credentials_manager = SpotifyClientCredentials('898c3a3775d944e2952d43a3b
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 def getArtist_id(artist_name):
-    #name = input("enter an artist name: ")
-    result = sp.search(artist_name) 
+    result = sp.search(artist_name)
     artist_id = result['tracks']['items'][0]['artists'][0]["id"]
-    #print(sp.artist_related_artists(artist_id))
+    return artist_id
     
-#def getAlbum_id():
-album_name = input("enter an album name: ") 
-result = sp.search(album_name)
-album_id = result['tracks']['items']
-print(album_id)
+def getAlbum_id(album_name):
+    result = sp.search(album_name)
+    album_id = result['tracks']['items']
+    return album_id
+    
+#def get_song_id(song_name):
+song_name = input('song name: ')
+result = sp.search(song_name)
+song_id = result
+print(song_id)
