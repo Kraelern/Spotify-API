8a3cd1d348d74e5db4f93ec88ca7a1bc75381c0d
rtan123 <taneranaga45@gmail.com>
2020-05-01 21:57:24 -0400

    commit

--- body.py
+++ body.py
@@ -5,10 +5,35 @@ from spotipy.oauth2 import SpotifyClientCredentials
 client_credentials_manager = SpotifyClientCredentials('898c3a3775d944e2952d43a3bc4dbd55', 'ba04bb9f22c344f99ba5eb3f8e3d9981')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-#user_choice = input('What would you like to do? Enter a number:\n1. ')
-mydict = {}
-output = get_id.get_album_id('Graduation')
-for key in output[1]:
-    c = output[1]['id']
-    mydict[c] = mydict.get(c, 0) + 1
-print(mydict)
+#getting the users three artists and appending them into a list
+user_artist_one = input('Enter three artists: \n1: ')
+user_artist_two = input('Enter a second artist: \n2: ')
+user_artist_three = input('Enter the third artist: \n3: ')
+user_artist_list = []
+user_artist_list.append(user_artist_one)
+user_artist_list.append(user_artist_two)
+user_artist_list.append(user_artist_three)
+
+#turning the user_artist_list from a string into the IDs
+user_artist_ID_1 = get_id.get_artist_id(user_artist_one)
+user_artist_ID_2 = get_id.get_artist_id(user_artist_two)
+user_artist_ID_3 = get_id.get_artist_id(user_artist_three)
+user_artist_IDs = []
+user_artist_IDs.append(user_artist_ID_1)
+user_artist_IDs.append(user_artist_ID_2)
+user_artist_IDs.append(user_artist_ID_3)
+
+#get three related artists
+def three_related_artists(artist_ID):
+    related_artists = sp.artist_related_artists(artist_ID)
+    count = 0
+    while count <= 2:
+        print(related_artists['artists'][count]['name'])
+        count += 1
+        
+user_nine_recs = []
+three_related_artists(user_artist_ID_1)
+three_related_artists(user_artist_ID_2)
+three_related_artists(user_artist_ID_3)
+
+print(sp.recommendations(user_artist_IDs))
--- module1.py
+++ module1.py
@@ -12,3 +12,26 @@ while playlists:
         playlists = sp.next(playlists)
     else:
         playlists = None
+
+#get first album search result id
+output = get_id.get_album_id('Graduation')
+albumdict = {}
+for key in output:
+    c = output['id']
+    albumdict[c] = albumdict.get(c)
+print(str(next(iter(albumdict))))
+
+#get artist id
+output2 = get_id.get_artist_id('Illenium')
+print(output2)
+
+#getting artist_related_artists to work
+related_artists = sp.artist_related_artists('69GGBxA162lTqCwzJG5jLp')#Illenium
+print(related_artists['artists'][0]['name'])
+
+#getting top three artist_related_artists to return
+related_artists = sp.artist_related_artists('69GGBxA162lTqCwzJG5jLp')#Illenium
+count = 0
+while count <= 2:
+    print(related_artists['artists'][count]['name'])
+    count += 1
