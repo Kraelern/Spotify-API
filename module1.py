14ed332a536c4880b9fc06a4aaac7e18765ae311
rtan123 <taneranaga45@gmail.com>
2020-05-10 16:16:06 -0400

    commit

--- module1.py
+++ module1.py
@@ -1,19 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import get_id
 
 client_credentials_manager = SpotifyClientCredentials('898c3a3775d944e2952d43a3bc4dbd55', 'ba04bb9f22c344f99ba5eb3f8e3d9981')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-playlists = sp.user_playlists('spotify')
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-<<<<<<< HEAD
-=======
 
 #get first album search result id
 output = get_id.get_album_id('Graduation')
@@ -37,4 +27,3 @@ count = 0
 while count <= 2:
     print(related_artists['artists'][count]['name'])
     count += 1
->>>>>>> 1e4c179f0154ffa8d9191d9a06e30062fd8f57a2
